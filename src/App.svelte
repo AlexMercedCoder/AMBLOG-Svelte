cfffa96a881d32de97972b3eac611ccfdf842cef
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-11-16 17:15:59 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Posts from "./Posts.svelte";
+</script>
+
+<main>
+	<h1>Welcome!</h1>
+	<p>This is another front-end application for my Contentful headless blog. This application was created using the Svelte compiler!</p>
+	<Posts/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Posts.svelte
@@ -0,0 +1,33 @@
+<div>
+
+{#each posts as post}
+	<div class="post">
+		<h1>{post.fields.blogTitle}</h1>
+		<p>{post.fields.blogContent}</p>
+	</div>
+{:else}
+	<!-- this block renders when photos.length === 0 -->
+	<p>loading...</p>
+{/each}
+
+</div>
+
+<script>
+import { onMount } from 'svelte';
+
+	let posts = [];
+
+	onMount(async () => {
+	const res = await fetch(`https://cdn.contentful.com/spaces/wiothdq64tqj/environments/master/entries?access_token=fbY3krPsiW8hWQ4m7GysjrfCfBdeCP9gBLqh3klbO20`);
+	let data = await res.json();
+	window.console.log(data);
+	posts = data.items;
+});
+</script>
+
+
+<style>
+
+.post {width: 80%; padding: 20px; background-color: #ff3e00; color: white;
+border-radius: 10px; margin: 20px auto;}
+</style>
